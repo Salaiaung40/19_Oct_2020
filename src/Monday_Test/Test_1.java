d09e1c90e7c0b03c851f48c3c366ea79640c55a8
Salaiaung40 <60382946+Salaiaung40@users.noreply.github.com>
2020-10-27 11:06:47 +0100

    second commit

--- src/Monday_Test/Test_1.java
+++ src/Monday_Test/Test_1.java
@@ -1,19 +1,19 @@
 package Monday_Test;
 
 public class Test_1 {
-    //    public static void main(String[] args) {
-//        for (int i = 1; i <= 100; i++) {
-//            if (i % 3 == 0)
-//                System.out.println("by 3: "+ i + ", ");
-//
-//            if (i % 5 == 0)
-//                System.out.println("by 5: "+ i + ", ");
-//
-//            if (i % 3 == 0 && i % 5 == 0)
-//                System.out.println("by 3 and 5: "+ i + ", ");
-//        }
-//
-//    }
+        public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0)
+                System.out.println("by 3: "+ i + ", ");
+
+            if (i % 5 == 0)
+                System.out.println("by 5: "+ i + ", ");
+
+            if (i % 3 == 0 && i % 5 == 0)
+                System.out.println("by 3 and 5: "+ i + ", ");
+        }
+
+    }
 
 // other way to write a code
 //    public static void main(String[] args) {
